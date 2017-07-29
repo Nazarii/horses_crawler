a9f76177ceeb98f03466beb71a1cefb2dccb7cf4
Nazarii Gudzovatyi <counter.sparta@gmail.com>
2017-07-30 01:39:01 +0300

    Basic scrapper

--- horses/pipelines.py
+++ horses/pipelines.py
@@ -4,9 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import os
+import csv
+import pandas as pd
 
 
 class HorsePipeline(object):
+
+    def open_spider(self, spider):
+        self.collection = []
+        self.df = pd.DataFrame()
+
+    def close_spider(self, spider):
+        self.df = pd.DataFrame(self.collection)
+        df = self.df
+        outpath = os.path.join(os.getcwd(), 'output.csv')
+        df.to_csv(outpath, index=False, quoting=csv.QUOTE_NONNUMERIC)
 
     def process_item(self, item, spider):
         self.collection.append(dict(item))
--- horses/spiders/horses_spider.py
+++ horses/spiders/horses_spider.py
@@ -1,6 +1,29 @@
 import scrapy
 from horses.items import HorseItem
 
+HORSES = ['Sizing John',
+          'Thistlecrack',
+          'Might Bite',
+          'Fox Norton',
+          'Douvan',
+          'Djakadam',
+          'Un De Sceaux',
+          'Outlander',
+          'Tea For Two',
+          'Native River',
+          'Cue Card',
+          'Coneygree',
+          'Yorkhill',
+          'Top Notch',
+          'Valseur Lido',
+          'Min',
+          'Altior',
+          'Bristol De Mai',
+          'Josses Hill',
+          'Bellshill',
+          'Clan Des Obeaux',
+          ]
+
 
 class HorsesSpider(scrapy.Spider):
     name = 'horses'
@@ -8,14 +31,23 @@ class HorsesSpider(scrapy.Spider):
     def start_requests(self):
         item = HorseItem()
         urls = [
-            'http://sports.williamhill.com/bet/en-gb/betting/e/11029917/ap/Ascot+-+King+George+VI+Stakes+-+29th+Jul+2017.html',
+            'http://sports.williamhill.com/bet/en-gb/betting/e/11029917/ap/Ascot+%2d+King+George+VI+Stakes+%2d+29th+Jul+2017.html',
             'https://www.skybet.com/horse-racing/ascot/event/20693785',
             'http://www.paddypower.com/racing/future-racing/king-george-vi--chase',
-        #     TODO - add Webdriver for click on next url
-            'https://www.bet365.com/?lng=1&amp;cb=105812028182#/HO/'
+            'http://www.paddypower.com/racing/future-racing/king-george-vi--chase',
+            'https://www.bet365.com/?lng=1&cb=105812028182#/AC/B2/C172/D101/E33281960/F2/G-1/H-1/P10/',
         ]
         for url in urls:
-            yield scrapy.Request(url=url, meta={'item': item}, callback=self.parse)
+            yield scrapy.Request(url=url, meta={'item': item, 'dont_redirect': True,
+                  'handle_httpstatus_list': [301, 302, 403]},  callback=self.parse)
 
     def parse(self, response):
-        pass
+        item = response.request.meta['item']
+        horse_lst = response.css('div.infos.left h4.left::text').extract()
+        horses = [str(horse.encode('ascii','ignore').strip('\n\t')) for horse in horse_lst]
+        odds = response.css('div.odd a::text').extract()
+        odds = map(str, odds)
+        item['horse_name'] = horses or HORSES
+        item['odds'] = odds or [0] *21
+        item['url'] = response.url or [''] * 21
+        yield item
