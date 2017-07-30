f9f8a21fa05af730bd77530610c1bc520fa707fe
Nazarii Gudzovatyi <counter.sparta@gmail.com>
2017-07-30 22:44:15 +0300

    Added DataFrame to print output

--- horses/pipelines.py
+++ horses/pipelines.py
@@ -4,21 +4,25 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import os
 import csv
+import os
 import pandas as pd
+from spiders.horses_spider import HORSES
 
 
 class HorsePipeline(object):
 
     def open_spider(self, spider):
         self.collection = []
-        self.df = pd.DataFrame()
 
     def close_spider(self, spider):
-        self.df = pd.DataFrame(self.collection)
-        df = self.df
         outpath = os.path.join(os.getcwd(), 'output.csv')
+        out = {}
+        odds = [x['odds'] for x in self.collection]
+        for horse in HORSES:
+            odd = [x.pop(0) for x in odds]
+            out[horse] = odd
+        df = pd.DataFrame.from_dict(out)
         df.to_csv(outpath, index=False, quoting=csv.QUOTE_NONNUMERIC)
 
     def process_item(self, item, spider):
